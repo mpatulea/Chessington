4cf3e28eedbe5574d921abe4512dcf7644bfac55
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:40:54 +0100

    completed Red-1 and Red-2

--- src/engine/board.ts
+++ src/engine/board.ts
@@ -1,26 +1,34 @@
 import Player from './player';
 import GameSettings from './gameSettings';
 import Square from './square';
-import Piece from './pieces/piece';
+import Piece from "./pieces/piece";
 
 export default class Board {
-    public currentPlayer: Player;
-    private readonly board: (Piece | undefined)[][];
+    currentPlayer;
+    board;
 
-    public constructor() {
-        this.currentPlayer = Player.WHITE;
+    constructor(currentPlayer: Player) {
+        this.currentPlayer = currentPlayer ? currentPlayer : Player.WHITE;
         this.board = this.createBoard();
     }
 
-    public setPiece(square: Square, piece: Piece | undefined) {
+    createBoard() {
+        const board = new Array(GameSettings.BOARD_SIZE);
+        for (let i = 0; i < board.length; i++) {
+            board[i] = new Array(GameSettings.BOARD_SIZE);
+        }
+        return board;
+    }
+
+    setPiece(square: Square, piece: Piece | undefined) {
         this.board[square.row][square.col] = piece;
     }
 
-    public getPiece(square: Square) {
+    getPiece(square: Square) {
         return this.board[square.row][square.col];
     }
 
-    public findPiece(pieceToFind: Piece) {
+    findPiece(pieceToFind: Piece) {
         for (let row = 0; row < this.board.length; row++) {
             for (let col = 0; col < this.board[row].length; col++) {
                 if (this.board[row][col] === pieceToFind) {
@@ -31,20 +39,12 @@ export default class Board {
         throw new Error('The supplied piece is not on the board');
     }
 
-    public movePiece(fromSquare: Square, toSquare: Square) {
+    movePiece(fromSquare: Square, toSquare: Square) {
         const movingPiece = this.getPiece(fromSquare);        
         if (!!movingPiece && movingPiece.player === this.currentPlayer) {
             this.setPiece(toSquare, movingPiece);
             this.setPiece(fromSquare, undefined);
             this.currentPlayer = (this.currentPlayer === Player.WHITE ? Player.BLACK : Player.WHITE);
         }
-    }
-
-    private createBoard() {
-        const board = new Array(GameSettings.BOARD_SIZE);
-        for (let i = 0; i < board.length; i++) {
-            board[i] = new Array(GameSettings.BOARD_SIZE);
-        }
-        return board;
     }
 }
--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -9,9 +9,30 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        let moves: Array<Square> = [];
-        moves.push(Square.at(1, 0));
-        moves.push(Square.at(6, 7));
+        let moves: Square[] = [];
+
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (board.board[i][j] && i == 1) {
+                    moves.push(Square.at(2, j));
+                    moves.push(Square.at(3, j));
+                } else if (board.board[i][j] && i == 6) {
+                    moves.push(Square.at(5, j));
+                    moves.push(Square.at(4, j));
+                } else if (board.board[i][j]) {
+                    if (this.player == Player.WHITE) {
+                        moves.push(Square.at(i + 1, j));
+                    } else {
+                        moves.push(Square.at(i - 1, j));
+                    }
+                }
+            }
+        }
+
         return moves;
+    }
+
+    public moveTo(board: Board, newSquare: Square) {
+        super.moveTo(board, newSquare);
     }
 }
