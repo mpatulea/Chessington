d4a84efecce8ff1a37f55b1119e1b74787594a24
Maria Patulea <Maria.Patulea@softwire.com>
2023-06-28 16:06:45 +0300

    added 2 available moves to pawns

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,9 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Array<Square> = [];
+        moves.push(Square.at(1, 0));
+        moves.push(Square.at(6, 7));
+        return moves;
     }
 }
