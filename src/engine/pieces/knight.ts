9576b2e2f26a1e8a6f12a4edeab340223e087fec
Maria Patulea <Maria.Patulea@softwire.com>
2023-06-29 11:06:14 +0300

    completed Red-6

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,25 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = [];
+
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (board.board[i][j]) {
+                    for (let k = i - 2; k <= i + 2; k++) {
+                        for (let t = j - 2; t <= j + 2; t++) {
+                            if (i == k || j == t || k == t || Math.abs(k - t) > 3 || (Math.abs(k-i) == 1 && Math.abs(t-j) == 1)) {
+                                continue;
+                            }
+                            console.log(k, t);
+                            moves.push(Square.at(k, t));
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        return moves;
     }
 }
