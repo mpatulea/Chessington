ecdd8521b0575308af1c4637778ac60d6ac3b71c
Maria Patulea <Maria.Patulea@softwire.com>
2023-06-29 11:12:10 +0300

    completed Red-7

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,24 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = [];
+
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (board.board[i][j]) {
+                    for (let k = i - 1; k <= i + 1; k++) {
+                        for (let t = j - 1; t <= j + 1; t++) {
+                            if (i == k && j == t) {
+                                continue;
+                            }
+                            moves.push(Square.at(k, t));
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        return moves;
     }
 }
