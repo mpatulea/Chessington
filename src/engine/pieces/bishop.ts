99a72b7a5ae8c0f9a4d68c4d642d11a48b205b43
Maria Patulea <Maria.Patulea@softwire.com>
2023-06-29 10:30:40 +0300

    completed Red-4

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,29 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = [];
+
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (board.board[i][j]) {
+                    for (let k = 0; k < 7; k++) {
+                        if (i == k && j == k + 1) {
+                            continue;
+                        }
+                        moves.push(Square.at(k, k + 1));
+                    }
+                    let sum = i + j;
+                    for (let k = 0; k <= sum; k ++) {
+                        if (i == k && j == sum - k) {
+                            continue;
+                        }
+                        moves.push(Square.at(k, sum - k));
+                    }
+                    break;
+                }
+            }
+        }
+
+        return moves;
     }
 }
